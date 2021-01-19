5729f07ba5eefdc462c7b850d8a2da55b808cd02
ToastCoder <vicky.pcbasic@gmail.com>
2021-01-19 20:29:53 +0530

    Some changes

--- pypi.sh
+++ pypi.sh
@@ -32,19 +32,19 @@ create_dist()
 up_testpypi()
 {
     $flag0 = 0
-    while [[ $flag0 -eq 0 ]]
+    while [[ $flag0 == 0 ]]
     do
         echo "TEST PYPI UPLOAD WIZARD"
         echo -n "Please check if your source code is bug-free before proceeding.Attempt to proceed(Y/N):  "
         read flag1
-        if [[ $flag1 -eq "y" ]] || [[ $flag2 -eq "Y" ]]
+        if [[ $flag1 == "y" || $flag2 == "Y" ]]
         then
             echo "Uploading to test pypi..."
             twine upload --repository testpypi --username $1 --password $2 dist/*
             echo "Uploaded to test pypi."
             echo "Please verify the test pypi package before proceeding to actual pypi package upload."
             $flag0 = 1
-        elif [[ $flag1 -eq "n" ]] || [[ $flag1 -eq "N" ]]
+        elif [[ $flag1 == "n" || $flag1 == "N" ]]
         then
             echo "Please verify the source code and try again."
         fi
@@ -54,18 +54,18 @@ up_testpypi()
 up_pypi()
 {
     $flag0 = 0
-    while [[ $flag0 -eq 0 ]]
+    while [[ $flag0 == 0 ]]
     do
         echo "PYPI UPLOAD WIZARD"
         echo -n "Please check if your test-pypi package is bug-free before proceeding.Attempt to proceed(Y/N):  "
         read flag1
-        if [[ $flag1 -eq "y" ]] || [[ $flag2 -eq "Y" ]]
+        if [[ $flag1 == "y" || $flag2 == "Y" ]]
         then
             echo "Uploading to pypi..."
             twine upload --username $1 --password $2 dist/*
             echo "Uploaded to pypi."
             $flag0 = 1
-        elif [[ $flag1 -eq "n" ]] || [[ $flag1 -eq "N" ]]
+        elif [[ $flag1 == "n" || $flag1 == "N" ]]
         then
             echo "Please verify the test-pypi package and try again."
         fi
@@ -83,14 +83,32 @@ echo -n "Password: "
 read -s pass
 echo " "
 REQ_FILE = setup.py
-if test -f "$REQ_FILE"
+if [ -f $REQ_FILE ]
 then
     echo "Checking dependencies and verifying their installation..."
     check_python
     check_pip
     create_dist
-    up_testpypi $name $pass
-    up_pypi $name $pass
+
+    echo -n "Do you want to upload to test-pypi?(Y/N): "
+    read opt1
+    if [[ $opt1 == 'Y' || $opt1 == 'y' ]]
+    then
+        up_testpypi $name $pass
+    elif [[ $opt1 == 'n' || $opt1 == 'N' ]]
+    then
+        echo "Test-pypi upload cancelled."
+    fi
+
+    echo -n "Do you want to upload to main pypi?(Y/N): "
+    read opt2
+    if [[ $opt2 == 'y' || $opt2 == 'Y' ]]
+    then
+        up_pypi $name $pass
+    elif [[ $opt2 == 'n' || $opt2 == 'N' ]]
+    then
+        echo "Pypi upload cancelled."
+    fi
 else
     echo "Unable to find setup.py in the current directory."
     echo "Please move the script to the directory where setup.py exists."
